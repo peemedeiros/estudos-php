305c65661f312827b3337169a5de43844cfb180f
Pedro Medeiros <pedrohenriquelm500@gmail.com>
2019-10-23 16:31:49 -0300

    adicionando o arquivo de importação do banco de dados

--- PWBE/adicionando BD/dumps/Dump20190902.sql
+++ PWBE/adicionando BD/dumps/Dump20190902.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `dbcontatos20192ta` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `dbcontatos20192ta`;
 -- MySQL dump 10.13  Distrib 8.0.11, for Win64 (x86_64)
 --
 -- Host: localhost    Database: dbcontatos20192ta
@@ -33,8 +31,10 @@ CREATE TABLE `tblcontatos` (
   `dt_nasc` date NOT NULL,
   `sexo` varchar(1) NOT NULL,
   `obs` longtext,
-  PRIMARY KEY (`codigo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `codestado` int(11) DEFAULT NULL,
+  PRIMARY KEY (`codigo`),
+  KEY `codestado_idx` (`codestado`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +43,33 @@ CREATE TABLE `tblcontatos` (
 
 LOCK TABLES `tblcontatos` WRITE;
 /*!40000 ALTER TABLE `tblcontatos` DISABLE KEYS */;
+INSERT INTO `tblcontatos` VALUES (11,'Jhonson','(111)1111-1111','11111111111','pedrohenriquelm500@gmail.com','2019-09-04','M','asdasdasd',NULL),(12,'Morpheus','(111)1111-1111','111111111','pedrohenriquelm500@gmail.com','2019-09-04','M','asdasdasdasdasd',2),(13,'Tux','1142062441','98888-8888','pedrohenriquelm500@gmail.com','1975-12-19','M','asdasdasdasdasd',2),(14,'Morpheus Matrix','1142062441','(011)95555-6666','pedrohenriquelm500@gmail.com','1990-03-10','M','asdsadasd',1);
 /*!40000 ALTER TABLE `tblcontatos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tblestado`
+--
+
+DROP TABLE IF EXISTS `tblestado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tblestado` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `sigla` varchar(2) DEFAULT NULL,
+  `descricao` text,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tblestado`
+--
+
+LOCK TABLES `tblestado` WRITE;
+/*!40000 ALTER TABLE `tblestado` DISABLE KEYS */;
+INSERT INTO `tblestado` VALUES (1,'SP','sp é meu pais'),(2,'RJ','terra de santa cruz');
+/*!40000 ALTER TABLE `tblestado` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -55,4 +81,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-09-02 16:16:19
+-- Dump completed on 2019-10-23 16:31:15
